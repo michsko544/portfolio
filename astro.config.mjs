2446fae3d2cbdc789d41b61ccd1f9066937a5b8c
Michał Skorus <michal.skorus@nubisoft.pl>
2024-06-22 13:00:15 +0200

    Improve GA loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
-import netlify from "@astrojs/netlify";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-    nesting: true
-  }), react()],
-  output: 'server',
-  adapter: netlify()
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+			nesting: true,
+		}),
+		react(),
+		partytown({ config: { forward: ['dataLayer.push'] } }),
+	],
+	output: 'server',
+	adapter: netlify(),
 });
