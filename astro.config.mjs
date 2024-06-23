2c8d33738352ec0b23f0f49a548cd2349f978f19
Michał Skorus <michal.skorus@nubisoft.pl>
2024-06-23 20:45:08 +0200

    Add robots.txt and sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,34 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
-
 import netlify from '@astrojs/netlify';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.michalskorus.pl',
 	integrations: [
 		tailwind({
 			applyBaseStyles: false,
 			nesting: true,
 		}),
 		react(),
-		partytown({ config: { forward: ['dataLayer.push'] } }),
+		partytown({
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+		sitemap({
+			serialize(item) {
+				if (item.url === 'https://www.michalskorus.pl/') {
+					item.changefreq = 'daily';
+					item.lastmod = new Date();
+					item.priority = 1;
+				}
+				return item;
+			},
+		}),
 	],
 	output: 'server',
 	adapter: netlify(),
