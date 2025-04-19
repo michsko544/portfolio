4f9a941551d1fb882b294810e102a9919180a39f
Michał Skorus <michal.skorus@nubisoft.pl>
2025-04-19 12:51:54 +0200

    Allow bots in robots.txt

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,6 +2,18 @@ import type { APIRoute } from 'astro';
 
 const robotsTxt = `
 User-agent: *
+Disallow:
+
+# Twitter preview bot
+User-agent: Twitterbot
+Allow: /
+
+# Facebook scraper
+User-agent: FacebookExternalHit
+Allow: /
+
+# LinkedIn bot
+User-agent: LinkedInBot
 Allow: /
 
 Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
